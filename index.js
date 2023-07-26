0fad26b2e9945d6318245e6684331c43a753f9ef
gidiasamoah <134442624+gidiasamoah@users.noreply.github.com>
2023-07-26 12:29:01 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -29,26 +29,21 @@ function fizzBuzz(n) {
 
 //Question 3
 //a. DOM (Document Object Model):
-//The DOM, short for Document Object Model, is a concept in web development that represents the structure and content of a web page as a tree-like structure. 
-//Every element on a web page, such as headings, paragraphs, images, buttons, etc., is represented as a node in this tree.
+// is a concept in web development that represents the structure and content of a web page as a tree-like structure. 
 // Through JavaScript, we can access and manipulate these nodes, allowing us to dynamically change the content, appearance, and behavior of the web page. The DOM serves as a bridge between the static HTML and dynamic JavaScript, enabling interactive and responsive web pages.
 
 //b. Promises:
-//Promises are a way of handling asynchronous operations in JavaScript. 
-//Imagine you have to wait for a server response or a file to load, and you don't know exactly when it will be available. 
+//Promises are a way of handling asynchronous operations in JavaScript.  
 //Promises provide a neat way to deal with this uncertainty. A promise represents the eventual completion (or failure) of an asynchronous task and acts as a placeholder for the result. It can be in three states: pending, fulfilled, or rejected. With promises, you can attach callback functions to be executed when the promise is resolved or rejected, allowing you to handle the results and errors in a structured and readable way.
 
 //c. Object-Oriented Programming (OOP):
 //Object-Oriented Programming (OOP) is a programming paradigm centered around the concept of objects. 
 //An object is like a container that holds data and the functions (methods) that operate on that data. 
-//It's like modeling real-world entities in code. OOP promotes the idea of creating blueprints for objects called classes, and then creating instances of those classes (objects) with specific properties and behaviors. 
-//This approach helps with code organization, reusability, and maintainability. OOP also introduces concepts like encapsulation, which hides the internal details of objects, and inheritance, where objects can inherit properties and behaviors from other objects.
+
 
 //d. Callback:
 //A callback is a function that is passed as an argument to another function and is executed at a later point in time or after a specific event occurs. 
-//It's like saying, "Hey, when you're done with that task, call this function to let me know." Callbacks are widely used in JavaScript, especially for handling asynchronous operations like making API calls, reading files, or responding to user interactions. 
-//They allow us to manage non-blocking operations efficiently and define what to do with the result once the operation is completed. Callbacks are essential for writing asynchronous code and controlling the flow of our programs.
-  
+//They allow us to manage non-blocking operations efficiently and define what to do with the result once the operation is completed.
   
 
 
